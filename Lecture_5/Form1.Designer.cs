602311f198fc8b0931f64a641699c0d3cf9f4b39
Lab321 Student01 <LAB321Student01@gau.local>
2025-03-24 11:55:02 +0400

    Lecture_5

---
+++ Lecture_5/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Lecture_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titletxt = new System.Windows.Forms.TextBox();
+            this.isbnTxt = new System.Windows.Forms.TextBox();
+            this.authorTxt = new System.Windows.Forms.TextBox();
+            this.priceTxt = new System.Windows.Forms.TextBox();
+            this.stockTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.insert_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.bookGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.bookGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titletxt
+            // 
+            this.titletxt.Location = new System.Drawing.Point(93, 60);
+            this.titletxt.Name = "titletxt";
+            this.titletxt.Size = new System.Drawing.Size(100, 20);
+            this.titletxt.TabIndex = 0;
+            // 
+            // isbnTxt
+            // 
+            this.isbnTxt.Location = new System.Drawing.Point(93, 146);
+            this.isbnTxt.Name = "isbnTxt";
+            this.isbnTxt.Size = new System.Drawing.Size(100, 20);
+            this.isbnTxt.TabIndex = 1;
+            // 
+            // authorTxt
+            // 
+            this.authorTxt.Location = new System.Drawing.Point(93, 104);
+            this.authorTxt.Name = "authorTxt";
+            this.authorTxt.Size = new System.Drawing.Size(100, 20);
+            this.authorTxt.TabIndex = 2;
+            // 
+            // priceTxt
+            // 
+            this.priceTxt.Location = new System.Drawing.Point(93, 191);
+            this.priceTxt.Name = "priceTxt";
+            this.priceTxt.Size = new System.Drawing.Size(100, 20);
+            this.priceTxt.TabIndex = 3;
+            // 
+            // stockTxt
+            // 
+            this.stockTxt.Location = new System.Drawing.Point(93, 234);
+            this.stockTxt.Name = "stockTxt";
+            this.stockTxt.Size = new System.Drawing.Size(100, 20);
+            this.stockTxt.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Author";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Stock";
+            // 
+            // insert_btn
+            // 
+            this.insert_btn.Location = new System.Drawing.Point(93, 308);
+            this.insert_btn.Name = "insert_btn";
+            this.insert_btn.Size = new System.Drawing.Size(75, 23);
+            this.insert_btn.TabIndex = 10;
+            this.insert_btn.Text = "Insert";
+            this.insert_btn.UseVisualStyleBackColor = true;
+            this.insert_btn.Click += new System.EventHandler(this.insert_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(315, 308);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(75, 23);
+            this.delete_btn.TabIndex = 11;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.Location = new System.Drawing.Point(531, 308);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(75, 23);
+            this.update_btn.TabIndex = 12;
+            this.update_btn.Text = "Update";
+            this.update_btn.UseVisualStyleBackColor = true;
+            this.update_btn.Click += new System.EventHandler(this.update_btn_Click);
+            // 
+            // bookGrid
+            // 
+            this.bookGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bookGrid.Location = new System.Drawing.Point(265, 60);
+            this.bookGrid.Name = "bookGrid";
+            this.bookGrid.Size = new System.Drawing.Size(380, 194);
+            this.bookGrid.TabIndex = 15;
+            this.bookGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.bookGrid_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 450);
+            this.Controls.Add(this.bookGrid);
+            this.Controls.Add(this.update_btn);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.insert_btn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stockTxt);
+            this.Controls.Add(this.priceTxt);
+            this.Controls.Add(this.authorTxt);
+            this.Controls.Add(this.isbnTxt);
+            this.Controls.Add(this.titletxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bookGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titletxt;
+        private System.Windows.Forms.TextBox isbnTxt;
+        private System.Windows.Forms.TextBox authorTxt;
+        private System.Windows.Forms.TextBox priceTxt;
+        private System.Windows.Forms.TextBox stockTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button insert_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.DataGridView bookGrid;
+    }
+}
+
---
+++ Lecture_5/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture_5
+{
+    public partial class Form1 : Form
+    {
+        private string _connectionString = "Server=LAB321PC01\\SQLEXPRESS02;Database=OnlineBookStore;Trusted_Connection=True;";
+        private SqlConnection _con;
+        int selectedBookID;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadData();
+        }
+
+        private async void insert_btn_Click(object sender, EventArgs e)
+        {
+            string title = titletxt.Text;
+            string author = authorTxt.Text;
+            string isbn = isbnTxt.Text;
+
+            decimal price = decimal.Parse(priceTxt.Text);
+            int stock = int.Parse(stockTxt.Text);
+
+            string query = "InsertBook";
+
+            try
+            {
+                using (_con = new SqlConnection(_connectionString))
+                {
+                    await _con.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand(query, _con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@Title", title);
+                        cmd.Parameters.AddWithValue("@Author", author);
+                        cmd.Parameters.AddWithValue("@ISBN", isbn);
+                        cmd.Parameters.AddWithValue("@Price", price);
+                        cmd.Parameters.AddWithValue("@Stock", stock);
+
+                        await cmd.ExecuteNonQueryAsync();
+                        MessageBox.Show("Successfully inserted the data");
+                        clearInputs();
+                        loadData();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void loadData()
+        {
+            string query = "Select * from dbo.Books";
+
+            try
+            {
+                using(SqlConnection _con = new SqlConnection(_connectionString))
+                {
+                    await _con.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand(query, _con))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+
+                            adapter.Fill(dt);
+
+                            bookGrid.DataSource = dt;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" +ex.Message);
+            }
+        }
+
+        private async void update_btn_Click(object sender, EventArgs e)
+        {
+            if (selectedBookID == -1)
+            {
+                MessageBox.Show("Please select a row to update");
+            }
+
+            string title = titletxt.Text;
+            string author = authorTxt.Text;
+            string isbn = isbnTxt.Text;
+
+            decimal price = decimal.Parse(priceTxt.Text);
+            int stock = int.Parse(stockTxt.Text);
+
+            string query = "UPDATE [dbo].[Books] SET [Title] =@Title, [Author] =@Author, [ISBN] = @ISBN, [Price] = @Price, [Stock] = @Stock WHERE BookID = @BookID";
+
+            try
+            {
+
+                using (SqlConnection _con = new SqlConnection(_connectionString))
+                {
+
+                    await _con.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand(query, _con))
+                    {
+                        cmd.Parameters.AddWithValue("@Title", title);
+                        cmd.Parameters.AddWithValue("@Author", author);
+                        cmd.Parameters.AddWithValue("@ISBN", isbn);
+                        cmd.Parameters.AddWithValue("@Price", price);
+                        cmd.Parameters.AddWithValue("@Stock", stock);
+                        cmd.Parameters.AddWithValue("@BookID", selectedBookID);
+
+
+                        await cmd.ExecuteNonQueryAsync();
+
+                        MessageBox.Show("Successfully updated the data");
+
+                        loadData();
+                        clearInputs();
+
+                        selectedBookID = -1;
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+
+        private void delete_btn_Click(object sender, EventArgs e)
+        {
+            if(selectedBookID == -1)
+            {
+                MessageBox.Show("Please select a row to delete");
+            }
+
+            string query = "DELETE FROM [dbo].[Books] WHERE BookID = @BookID";
+
+            try
+            {
+                using(SqlConnection _con = new SqlConnection(_connectionString))
+                {
+                    _con.OpenAsync();
+
+                    using (SqlCommand cmd = new SqlCommand(query, _con))
+                    {
+                        DbParameter param = cmd.CreateParameter();
+                        param.ParameterName = "@BookID";
+                        param.Value = selectedBookID;
+                        param.DbType = System.Data.DbType.Int32;
+
+                        cmd.Parameters.Add(param);
+
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Book deleted successfully!");
+
+                        loadData();
+                        clearInputs();
+
+                    }
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error"+ex.Message);
+            }
+
+        }
+
+        private void bookGrid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow row = bookGrid.Rows[e.RowIndex];
+
+                titletxt.Text = row.Cells["Title"].Value.ToString();
+                authorTxt.Text = row.Cells["Author"].Value.ToString();
+                isbnTxt.Text = row.Cells["ISBN"].Value.ToString();
+                priceTxt.Text = row.Cells["Price"].Value.ToString();
+                stockTxt.Text = row.Cells["Stock"].Value.ToString();
+
+                selectedBookID = Convert.ToInt32(row.Cells["BookID"].Value);
+            }
+        }
+
+        private void clearInputs()
+        {
+            titletxt.Clear();
+            authorTxt.Clear();
+            isbnTxt.Clear();
+            priceTxt.Clear();
+            stockTxt.Clear();
+        }
+
+        
+    }
+}
